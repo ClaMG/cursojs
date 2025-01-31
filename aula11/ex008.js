534ea33d89991ac651baf74e7dc1108b4e886cb3
Cla <157390340+ClaMG@users.noreply.github.com>
2025-01-30 21:21:33 -0300

    pra porra

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,1 +1,8 @@
-console.log('deu bom')
+vel = 60.5 
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+
+if(vel>60){
+    console.log(`você vai ser multado`)
+}
+
+console.log(`Dirija com segurança`)
---
+++ aula11/ex009.js
@@ -0,0 +1,9 @@
+pais ='Brasil'
+
+console.log(`Vivendo em ${pais}`)
+
+if(pais != 'Brasil'){
+    console.log('Você é Estrangeiro');
+} else{
+    console.log('Você é BR de cria');
+}
